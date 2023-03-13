fab3ef575875030057312cec0b2ba4a461bc7fb4
DevSamuelNwankwo <73386074+DevSamuelNwankwo@users.noreply.github.com>
2023-03-13 19:18:32 +0100

    first product

---
+++ src/Header.js
@@ -0,0 +1,11 @@
+import './Header.css'
+
+const Header = () => {
+    return ( 
+        <div>
+            <h1> Fun Facts About The World</h1>
+        </div>
+    );
+}
+ 
+export default Header;
---
+++ src/Strangefacts.js
@@ -0,0 +1,27 @@
+import './sf.css'
+
+const Strangefacts = () => {
+    return (
+        <div className='container-sm'>
+             <ul>
+             <li>The shortest war in history was between Britain and Zanzibar in 1896. It lasted only 38 minutes.</li> 
+              <li>The world's largest snowflake on record was 15 inches wide and 8 inches thick, and fell in Fort Keogh, Montana in 1887. </li> 
+              <li>The oldest known living organism is a tree named "Methuselah" that is over 4,800 years old and grows in California's White Mountains. </li> 
+              <li>The world's largest river, the Amazon River, discharges more water than the next seven largest rivers combined. </li> 
+              <li>The highest mountain in our solar system is Olympus Mons on Mars, which stands at a towering 22 kilometers (14 miles) tall. </li>
+              <li> The largest desert in the world is actually the continent of Antarctica, which is considered a polar desert.</li>
+              <li> The longest officially recognized place name in the world is Taumatawhakatangihangakoauauotamateaturipukakapikimaungahoronukupokaiwhenuakitanatahu, a hill in New Zealand. </li>
+              <li> The only continent without any reptiles or snakes is Antarctica.</li>
+              <li>The world's oldest piece of chewing gum is over 9,000 years old and was found in Sweden. </li>
+              <li> The deepest point in the ocean is the Challenger Deep in the Mariana Trench, which is over 11 kilometers (7 miles) deep. </li>
+              <li>The country with the most languages spoken is Papua New Guinea, with over 820 different languages spoken. </li>
+              <li> The first person to reach the South Pole was Roald Amundsen of Norway in 1911.</li>
+              <li> The coldest temperature ever recorded on Earth was -128.6 degrees Fahrenheit (-89.2 degrees Celsius) at the Soviet Union's Vostok Station in Antarctica on July 21, 1983. </li>
+              <li> The shortest scheduled commercial flight in the world is between the islands of Westray and Papa Westray in Scotland, which takes only 2 minutes.</li>
+              <li>The highest temperature ever recorded on Earth was 134 degrees Fahrenheit (56.7 degrees Celsius) in Death Valley, California, USA on July 10, 1913.</li>
+             </ul>
+        </div>
+    );
+}
+
+export default Strangefacts;
